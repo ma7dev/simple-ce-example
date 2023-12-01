ae492d8c7524f2330b58d8c094d05334b43bd155
Mazen Alotaibi <mazen.alotaibi@malaa.tech>
2023-12-01 19:01:34 +0300

    fix

--- run.py
+++ run.py
@@ -3,14 +3,17 @@ import os
 import random
 import string
 
-def my_train_func():
+def func():
     machine_num = os.environ.get("MACHINE_NUM")
     random_string=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
     wandb.init(group=machine_num, name=f"{machine_num}-{random_string}")
-    a = wandb.config.a
-
-    wandb.log({"a": a, "accuracy": a + 1})
+    
+    x = wandb.config.x
+    
+    acc = x+1
+    
+    wandb.log({"x": x, "accuracy": acc})
 
 
 if __name__ == "__main__":
-    my_train_func()
+    func()
