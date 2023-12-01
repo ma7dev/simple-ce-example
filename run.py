52b5de51d6523f7e6dfa0a7eca83ca8557b6c8d0
Mazen Alotaibi <mazen.alotaibi@malaa.tech>
2023-12-01 19:28:53 +0300

    fix

--- run.py
+++ run.py
@@ -4,14 +4,25 @@ import random
 import string
 
 def func():
-    machine_num = os.environ.get("MACHINE_NUM")
-    random_string=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
-    wandb.init(name=f"{machine_num}-{random_string}")
+    # get machine number
+    machine_num = os.environ.get("MACHINE_NUM") or "0"
     
+    # get random 5 character string
+    random_string=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+    
+    # set experiment name
+    exp_name = f"{machine_num}-{random_string}"
+    
+    # initialize experiment
+    wandb.init(name=exp_name)
+    
+    # get x from config
     x = wandb.config.x
     
+    # calculate accuracy
     acc = abs(x - 0.5)
     
+    # log x and accuracy to wandb
     wandb.log({"x": x, "accuracy": acc})
 
 
--- start.py
+++ start.py
@@ -2,11 +2,14 @@ import wandb
 import yaml
 
 if __name__ == "__main__":
+    # load config
     with open("config.yml", 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     print(config)
-    entity = config['entity']
-    project = config['project']    
+    
+    # initialize sweep
     sweep_id = wandb.sweep(config)
-    sweep_url = f"{entity}/{project}/{sweep_id}"
+    
+    # get sweep url
+    sweep_url = f"{config['entity']}/{config['project']}/{sweep_id}"
     print(sweep_url)
